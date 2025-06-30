50025dbc0a3513c5d3d740479541970dd3192395
Mark Wolfe <mark.wolfe@buildkite.com>
2025-07-01 09:32:53 +1000

    fix: skip docker login if creds aren't available

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -10,8 +10,17 @@ set -uo pipefail
 export GORELEASER_KEY=""
 GORELEASER_KEY=$(buildkite-agent secret get goreleaser_key)
 
-echo "--- :key: :docker: Login to Docker"
-echo "${DOCKERHUB_PASSWORD}" | docker login --username "${DOCKERHUB_USER}" --password-stdin
+# check if DOCKERHUB_USER and DOCKERHUB_PASSWORD are set if not skip docker login
+if [[ -z "${DOCKERHUB_USER:-}" || -z "${DOCKERHUB_PASSWORD:-}" ]]; then
+    echo "Skipping Docker login as DOCKERHUB_USER or DOCKERHUB_PASSWORD is not set"
+else
+    echo "--- :key: :docker: Login to Docker"
+    echo "${DOCKERHUB_PASSWORD}" | docker login --username "${DOCKERHUB_USER}" --password-stdin
+    if [[ $? -ne 0 ]]; then
+        echo "Docker login failed"
+        exit 1
+    fi
+fi
 
 if [[ $? -ne 0 ]]; then
     echo "Failed to retrieve GoReleaser Pro key"
