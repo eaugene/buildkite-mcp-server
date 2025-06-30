97ac6a5bb886df8ffa9461418843cb5650345e60
Mark Wolfe <mark.wolfe@buildkite.com>
2025-06-30 17:49:32 +1000

    chore: add support for docker hub and push an image using ko
    This also sorts out some copy paste issues in the release pipeline.

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -10,6 +10,9 @@ set -uo pipefail
 export GORELEASER_KEY=""
 GORELEASER_KEY=$(buildkite-agent secret get goreleaser_key)
 
+echo "--- :key: :docker: Login to Docker"
+echo "${DOCKERHUB_PASSWORD}" | docker login --username "${DOCKERHUB_USER}" --password-stdin
+
 if [[ $? -ne 0 ]]; then
     echo "Failed to retrieve GoReleaser Pro key"
     exit 1
