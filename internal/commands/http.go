9efbff42057379bf4baa81d6f696f4bf0e93be2f
Chris Atkins <chris.a@buildkite.com>
2025-08-14 12:51:04 +1000

    listen on localhost, but document risks of binding on 0.0.0.0

--- internal/commands/http.go
+++ internal/commands/http.go
@@ -3,32 +3,44 @@ package commands
 import (
 	"context"
 	"fmt"
+	"net"
+	"net/http"
 
 	"github.com/buildkite/buildkite-mcp-server/pkg/server"
 	mcpserver "github.com/mark3labs/mcp-go/server"
 	"github.com/rs/zerolog/log"
 )
 
+const defaultHTTPPort = 3000
+
 type HTTPCmd struct {
-	Listen string `help:"The address to listen on." default:"localhost:3000"`
+	Listen string `help:"The address to listen on." default:"localhost:3000" env:"HTTP_LISTEN_ADDR"`
 	UseSSE bool   `help:"Use deprecated SSS transport instead of Streamable HTTP." default:"false"`
 }
 
 func (c *HTTPCmd) Run(ctx context.Context, globals *Globals) error {
 	mcpServer := server.NewMCPServer(globals.Version, globals.Client, globals.BuildkiteLogsClient)
+
+	listener, err := net.Listen("tcp", c.Listen)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %s: %w", c.Listen, err)
+	}
 	logEvent := log.Ctx(ctx).Info().Str("address", c.Listen)
 
-	if c.UseSSE {
-		httpServer := mcpserver.NewSSEServer(mcpServer)
-		endpoint := fmt.Sprintf("http://%s%s", c.Listen, httpServer.CompleteSsePath())
-		logEvent.Str("transport", "sse").Str("endpoint", endpoint).Msg("Starting SSE HTTP server")
-
-		return httpServer.Start(c.Listen)
-	} else {
-		httpServer := mcpserver.NewStreamableHTTPServer(mcpServer)
-		endpoint := fmt.Sprintf("http://%s/mcp", c.Listen)
-		logEvent.Str("transport", "streamable-http").Str("endpoint", endpoint).Msg("Starting streamable HTTP server")
-
-		return httpServer.Start(c.Listen)
+	mux := http.NewServeMux()
+	srv := http.Server{
+		Handler: mux,
 	}
+
+	if c.UseSSE {
+		handler := mcpserver.NewSSEServer(mcpServer)
+		mux.Handle("/sse", handler)
+		logEvent.Str("transport", "sse").Str("endpoint", fmt.Sprintf("http://%s/sse", listener.Addr())).Msg("Starting SSE HTTP server")
+	} else {
+		handler := mcpserver.NewStreamableHTTPServer(mcpServer)
+		mux.Handle("/mcp", handler)
+		logEvent.Str("transport", "streamable-http").Str("endpoint", fmt.Sprintf("http://%s/mcp", listener.Addr())).Msg("Starting Streamable HTTP server")
+	}
+
+	return srv.Serve(listener)
 }
